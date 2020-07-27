5407a380c4c7c97cf6afda2d0f51ede276526c8c
Noelle Everage <neverage84@yahoo.com>
2020-07-27 14:22:28 -0700

    outline of EmailApp

---
+++ JavaProjects/src/emailapp/Email.java
@@ -0,0 +1,26 @@
+package emailapp;
+
+public class Email {
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternativeEmail;
+	
+	// Need a constructor to receive the first name and last name
+	
+	//Ask for the department
+	
+	//Generate a random password
+	
+	//Set the mailbox capacity
+	
+	//Set the alternative email
+	
+	//Change the password
+	
+	
+
+}
