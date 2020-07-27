8063f3a844931479dcbe00ff0d4f0373898e9b08
Noelle Everage <neverage84@yahoo.com>
2020-07-27 16:37:07 -0700

    Added email set up

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -8,8 +8,12 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
+	private String email;
+	private int defaultPasswordLength = 10;
 	private int mailboxCapacity;
 	private String alternativeEmail;
+	private String companySuffix = "aeycompany.com";
+	
 	
 	// Need a constructor to receive the first name and last name
 	public Email (String firstName, String lastName) {
@@ -19,6 +23,13 @@ public class Email {
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
 		System.out.printf("Department is: %s%n", this.department);
+		
+		//Call a method that returns a random password
+		this.password = this.randomPassword(defaultPasswordLength);
+		System.out.printf("You new random password is %s%n", password);
+		//Combine elements to generate email
+		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+		System.out.printf("Your email is: %s%n", email );
 	}
 	//Ask for the department
 	private String setDepartment() {
@@ -35,13 +46,23 @@ public class Email {
 			return "accounting";
 		default:
 			return " ";
-		}
-		
-		
-			
+		}	
 	}
 	
 	//Generate a random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ10234567890!@#$%^&*()";
+		char[]password = new char[length];
+		for (int i = 0; i < length; i++) {
+			int rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
+		
+	}
+	
+	
+	
 	
 	//Set the mailbox capacity
 	
--- JavaProjects/src/emailapp/EmailApp.java
+++ JavaProjects/src/emailapp/EmailApp.java
@@ -5,6 +5,8 @@ public class EmailApp {
 	public static void main(String[] args) {
 	
 		Email em1 = new Email("John", "Smith");
+		
+		
 	}
 
 }
