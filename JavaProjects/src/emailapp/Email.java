3cc1034d07c270905990b652bc1b5cd17ce93753
Noelle Everage <neverage84@yahoo.com>
2020-07-27 15:41:02 -0700

    Added switch statement to handle department selection

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	
 	private String firstName;
@@ -10,8 +12,34 @@ public class Email {
 	private String alternativeEmail;
 	
 	// Need a constructor to receive the first name and last name
-	
+	public Email (String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.printf("EMAIL CREATED: %s %s %n", this.firstName, this.lastName);
+		// Call a method asking for the department - return the department
+		this.department = setDepartment();
+		System.out.printf("Department is: %s%n", this.department);
+	}
 	//Ask for the department
+	private String setDepartment() {
+		System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n 0 for none");
+		Scanner scanner = new Scanner(System.in);
+		int depChoice = scanner.nextInt();
+		scanner.close();
+		switch (depChoice) {
+		case 1:
+			return "sales";
+		case 2:
+			return "development";
+		case 3:
+			return "accounting";
+		default:
+			return " ";
+		}
+		
+		
+			
+	}
 	
 	//Generate a random password
 	
