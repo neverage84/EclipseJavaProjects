a1c97db6c1678f2e7716dee05e4b357c8dd89314
Noelle Everage <neverage84@yahoo.com>
2020-07-27 18:39:49 -0700

    Added switch for class level

--- JavaProjects/src/studentsdatabaseapp/Student.java
+++ JavaProjects/src/studentsdatabaseapp/Student.java
@@ -7,6 +7,7 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
+	private String classYear;
 	private int studentID;
 	private String courses;
 	private int tuitionBalance;
@@ -15,15 +16,32 @@ public class Student {
 	//Constructor: prompt user to enter student's name and year
 	public Student() {
 		Scanner in = new Scanner(System.in);
+		
 		System.out.println("Enter student First Name");
 		this.firstName = in.nextLine();
-		System.out.println("Enter studen Last Name");
+		
+		System.out.println("Enter student Last Name");
 		this.lastName = in.nextLine();
 		
 		
 		System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter class level");
 		this.gradeYear = in.nextInt();
-		System.out.printf("First Name: %s%n Last Name:%s%n Grade Year: %s%n", this.firstName, this.lastName, this.gradeYear);
+		this.classYear = this.getClassYear(this.gradeYear);
+		System.out.printf("First Name: %s%n Last Name:%s%n Grade: %s%n", this.firstName, this.lastName, this.classYear);
+	}
+	
+	private String getClassYear(int gradeYear) {
+		switch (gradeYear) {
+		case 1:
+			return "Freshman";
+		case 2:
+			return "Sophomore";
+		case 3:
+			return "Junior";
+		default:
+			return "Senior";
+		}
+	}
 	//Generate an ID
 	
 	//Enroll in courses
--- JavaProjects/src/studentsdatabaseapp/StudentDatabaseApp.java
+++ JavaProjects/src/studentsdatabaseapp/StudentDatabaseApp.java
@@ -3,8 +3,10 @@ package studentsdatabaseapp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Ask how many new users we want to add
+		Student student1 = new Student();
+		
+		//Create n number of new students
 	}
 
 }
