71b28031385bccfd4073ea10222d0997f15b5d64
Noelle Everage <neverage84@yahoo.com>
2020-07-27 18:27:27 -0700

    Prompt for student name and year assigned to variables

--- JavaProjects/src/studentsdatabaseapp/Student.java
+++ JavaProjects/src/studentsdatabaseapp/Student.java
@@ -1,16 +1,29 @@
 package studentsdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 	
 	private String firstName;
 	private String lastName;
-	private String gradeYear;
+	private int gradeYear;
 	private int studentID;
 	private String courses;
 	private int tuitionBalance;
 	private int costOfCourse = 600;
-	//Constructor: prompt user to enter student's name and year
 	
+	//Constructor: prompt user to enter student's name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter student First Name");
+		this.firstName = in.nextLine();
+		System.out.println("Enter studen Last Name");
+		this.lastName = in.nextLine();
+		
+		
+		System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter class level");
+		this.gradeYear = in.nextInt();
+		System.out.printf("First Name: %s%n Last Name:%s%n Grade Year: %s%n", this.firstName, this.lastName, this.gradeYear);
 	//Generate an ID
 	
 	//Enroll in courses
